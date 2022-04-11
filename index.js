fa177a095b4884f9d163f2d68ee22dca0feef5cc
MarvellousObiwulu <marvheloz3@gmail.com>
2022-04-11 19:25:29 +0100

    Second js Assignment

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
-("0");
-{alert('hello')}
+let age = +prompt('what is your age');
+console.log (typeof age)
+
+// switch(age){
+//   case 10:
+//     console.log('you can play the game')
+//     break;
+//     case 15:
+//     console.log('you can see a movie')
+//     break;
+//     case 23:
+//     console.log('you can grab a coffee')
+//     break;s
+//     case 30:
+//     console.log('you can fuck off')
+//     break;
+//     default:
+//     console.log('you are not involved')
+// }
+if (age == 10){
+  console.log('you can play the game')
+}
+else if (age == 15){
+  console.log('you see a movie')
+}
+else if (age == 23){
+  console.log('you see a grab a coffee')
+}
+else if (age == 30){
+  console.log('you can fuck off')
+}
+else{
+  console.log('you are not involved')
+}
